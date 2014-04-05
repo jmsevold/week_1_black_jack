5709f3fd20d28c00ad002caecf8d23f1b757a4f0
Jonathan Sevold <Jmsevold@gmail.com>
2014-04-05 12:59:15 -0700

    fleshed out game, not tested yet

--- week_1_black_jack.rb
+++ week_1_black_jack.rb
@@ -51,39 +51,6 @@ $deck = []
 
 
 
-#create combos of faces & suits
-faces = %w{ 2 3 4 5 6 7 8 9 10 Jack King Queen Ace }
-
-suits = %w{ Hearts Spades Diamonds Clubs}
-
-
-
-#determine value based on face for cards
-def card_value_determiner(card)
-  string_nums = (2..10).to_a.map { |i| i.to_s}
-  if string_nums.include?(card.face)
-    card.value = card.face.to_i
-  else
-    card.value = 10
-  end
-end
-
-
-
-#create all 52 cards,assign values, & populate deck
-faces.each do |face|
-  suits.each do |suit|
-    c = Card.new(face,suit)
-    card_value_determiner(c)
-    $deck << c
-  end
-end
-
-
-$deck.shuffle
-
-
-
 
 
 
@@ -96,6 +63,60 @@ def win_or_bust(player)
       puts "#{player.player_name} busted and loses!"
       player.bust = true
    end
+end
+
+#popping, checking, and pushing to player's hand
+def deal_card(deck,player)
+  card = deck.pop
+  ace_checker(card,player)
+  player.hand.push(card)
+  puts"#{player.player_name} received a #{card.identify}"
+  puts "Current hand: "
+  player.display_hand
+  win_or_bust(player)
+  hit_or_stay(player)
+end
+
+
+def deal_to_dealer(player)
+  card = deck.pop
+  ace_checker(card,player)
+  player.hand.push(card)
+  puts"#{player.player_name} received a #{card.identify}"
+  puts "Current hand: "
+  player.display_hand
+  win_or_bust(player)
+  dealer_hit_or_stay(player)
+end
+
+
+def compare_hands(player1,player2)
+  if player1.calculate_total > player2.calculate_total
+    puts "#{player1.player_name} has #{player1.calculate_total} and wins the game."
+    player1.won = true
+  elsif player1.calculate_total < player2.calculate_total
+    puts "#{player2.player_name} has #{player2.calculate_total} and wins the game."
+    player2.won = true
+  else
+    puts "Tie game! Dealer wins!"
+    dealer.won = true
+    break
+  end
+end
+
+
+
+def dealer_hit_or_stay(dealer)
+stay = false
+  until stay == true
+  puts "Will #{dealer.player_name} hit or stay?"
+  answer = gets.chomp.downcase
+  if answer == "hit"
+    deal_card($deck,dealer)
+  elsif answer == 'stay'
+    stay == true
+  end
+  compare_hands(user,dealer)
 end
 
 
@@ -114,52 +135,84 @@ def hit_or_stay(player)
 end
 
 
-def first_deal($deck,player)
-  c1 = $deck.pop
-  c2 = $deck.pop
+def first_deal(deck,player)
+  c1 = deck.pop
+  c2 = deck.pop
+  ace_checker(c1)
+  ace_checker(c2)
   player.hand.push(c1,c2)
-  puts"#{player.player_name} received a #{player.hand.last.identify}"
-  puts"#{player.player_name} received a #{player.hand[-2].identify}"
+  puts"#{player.player_name} received a #{c1.identify}"
+  puts"#{player.player_name} received a #{c2.identify}"
   puts "Current hand: "
   player.display_hand
   win_or_bust(player)
   hit_or_stay(player)
 end
 
+  
+def ace_checker(card,player)
+  if card.face == "Ace" && player.calculate_total + 11 > 21
+    card.value = 1
+  elsif card.face == "Ace" && player.calculate_total + 11 < 21
+    card.value == 11
+  end
+end
+    
 
 
-def deal_card($deck, player)
-  card = $deck.pop
-  player.hand.push(card)
-  puts"#{player.player_name} received a #{player.hand.last.identify}"
-  puts "Current hand: "
-  player.display_hand
-  win_or_bust(player)
-  hit_or_stay(player)
+
+
+
+def first_dealer_deal(player)
+  until player.hand >= 17
+    card = deck.pop
+    ace_checker(card,player)
+    player.hand.push(card)
+    puts"#{player.player_name} received a #{card.identify}"
+    puts "Current hand: "
+    player.display_hand
+    win_or_bust(player)
+  end
+  dealer_hit_or_stay(player)
 end
 
 
 
-   
-def ace_prompt(card,player)
-  if card.face == "Ace"
-    puts "Would #{player.player_name} like this Ace to be worth 11 or 1?"
-    answer = gets.chomp
-    if answer == '1'
-      card.value = 1
-    elsif answer == '11'
-      card.value == 11
-    end
+#create deck
+#create combos of faces & suits
+faces = %w{ 2 3 4 5 6 7 8 9 10 Jack King Queen Ace }
+
+suits = %w{ Hearts Spades Diamonds Clubs}
+
+
+
+#determine value based on face for cards
+def card_value_determiner(card)
+  string_nums = (2..10).to_a.map { |i| i.to_s}
+  if string_nums.include?(card.face)
+    card.value = card.face.to_i
+  elsif card.face == 'Ace'
+    card.value == 11
+  else
+    card.value = 10
   end
 end
 
 
-until game_over?(user) || game_over?(dealer)
+
+#create all 52 cards,assign values, & populate deck
+faces.each do |face|
+  suits.each do |suit|
+    c = Card.new(face,suit)
+    card_value_determiner(c)
+    $deck << c
+  end
 end
-puts "Thank you for playing!"
 
 
-  
+$deck.shuffle!
+
+#create players
 user = Player.new
 dealer = Player.new
 dealer.player_name = "Dealer"
@@ -167,6 +220,15 @@ puts "Welcome to Blackjack! What is your name?"
 name = gets.chomp
 user.player_name = name
 puts "Let's get started!"
+
+
+until game_over?(user) || game_over?(dealer)
+  first_deal($deck,user)
+  first_dealer_deal(dealer)
+end
+puts "Thank you for playing!" 
+
+
 
 
 
